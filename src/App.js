16a302a70b0c681b5e29dab276c45f99e9bc67e9
Mayank Goyal <mayankg1908@gmail.com>
2022-08-12 14:38:42 +0530

    count-down timer completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import hills from "./images/pattern-hills.svg";
+import stars from "./images/bg-stars.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      day: '',
+      hrs: '',
+      mins: '',
+      secs: '',
+    };
+  }
+
+  timer = () => {
+    const d1 = new Date();
+    const d2 = new Date(2023, 7, 6, 19, 30, 0);
+    const d = d2 - d1;
+    const days = String(Math.trunc(d / 1000 / 60 / 60 / 24)).padStart(2,0);
+    const hrs = String(Math.trunc((d / 1000 / 60 / 60) % 24)).padStart(2,0);
+    const mins = String(Math.trunc((d / 1000 / 60) % 60)).padStart(2,0);
+    const secs = String(Math.trunc((d / 1000) % 60)).padStart(2,0);
+    this.setState(() => {
+      return { days, hrs, mins, secs };
+    });
+  };
+  render() {
+    const { days, hrs, mins, secs } = this.state;
+    setInterval(this.timer, 1000);
+    return (
+      <div>
+        <h1 className="title">We're launching soon</h1>
+        <div className="count-down">
+          <div className="count">{days}</div>
+          <div className="count">{hrs}</div>
+          <div className="count">{mins}</div>
+          <div className="count">{secs}</div>
+          <label>DAYS</label>
+          <label>HOURS</label>
+          <label>MINUTES</label>
+          <label>SECONDS</label>
+        </div>
+        <img className="hills" src={hills} alt="" />
+      </div>
+    );
+  }
 }
 
 export default App;
